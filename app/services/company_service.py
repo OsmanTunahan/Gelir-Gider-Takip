ee702865f36ca2ec645351ffb8e7e4b472e7cc9e
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-01 23:47:56 +0300

    feat(company_service): add_company function

--- app/services/company_service.py
+++ app/services/company_service.py
@@ -1,2 +1,9 @@
 from db.database import get_connection
 from app.models.company import Company
+
+def add_company(name):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO companies (name) VALUES (?)", (name,))
+        conn.commit()
+        return cursor.lastrowid
