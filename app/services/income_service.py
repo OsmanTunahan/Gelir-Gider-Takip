19b7244a76b8f29d1e1985a7dcc4618122cec038
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-01 23:43:04 +0300

    feat(income_service): add add_income function

--- app/services/income_service.py
+++ app/services/income_service.py
@@ -1,3 +1,19 @@
 from db.database import get_connection
 from models.income import Income
 import mysql.connector
+
+def add_income(amount, date, description=None):
+    conn = get_connection()
+    if conn is None:
+        return None
+
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO income (amount, date, description) VALUES (%s, %s, %s)", (amount, date, description))
+        conn.commit()
+        return cursor.lastrowid
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        conn.close()
