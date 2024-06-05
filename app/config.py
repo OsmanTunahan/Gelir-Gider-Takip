116d5360438c0c3bb442c6f4d139146a66378b2c
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-05 16:19:35 +0300

    first commit x)

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    DATABASE_HOST = os.environ.get('DATABASE_HOST') or 'localhost'
-    DATABASE_NAME = os.environ.get('DATABASE_NAME') or 'gelirgider'
-    DATABASE_USER = os.environ.get('DATABASE_USER') or 'root'
-    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD') or ''
--- app/models/income.py
+++
@@ -1,9 +0,0 @@
-class Income:
-    def __init__(self, id, amount, date, description):
-        self.id = id
-        self.amount = amount
-        self.date = date
-        self.description = description
-
-    def __repr__(self):
-        return f"Income(id={self.id}, amount={self.amount}, date='{self.date}', description='{self.description}')"
